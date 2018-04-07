6bc2a96823bf092285cc4924cb9cfe3e67c271b7
Daniella D <danielladevoto@gmail.com>
2018-04-07 13:49:28 -0400

    working on scimuse api

--- server.js
+++ server.js
@@ -2,22 +2,26 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const models = require('./models');
+const cors = require('cors');
 
 const app = express();
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://127.0.0.1/astroids";
 const PORT = process.env.PORT || 3001;
 const routes = require('./routes')
 
+//app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:3000',
+  credentials: true,
+}));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("client/build"));
 app.use(routes);
 
-
 mongoose.Promise = global.Promise;
 mongoose.connect(MONGODB_URI);
-
-
 
 app.listen(PORT, function() {
   console.log("App running on port " + PORT + "!");
