6ec667b9458fce9aa0ecffa50dec33407edab716
Alex Beim <alexandergbeim@gmail.com>
2018-04-10 19:43:32 -0400

    created basic react files and folders for profile and collapse

---
+++ client/src/components/collapse/collapse.js
@@ -0,0 +1,5 @@
+const Collapse = (props) => {
+  return (
+    
+  )
+}
---
+++ client/src/pages/Profile/Profile.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import styles from './Profile.css';
+import Collapse from '../../components/collapse';
+
+class Profile extends Component {
+  render() {
+    return (
+
+    )
+  }
+}
+
+export default Profile;
