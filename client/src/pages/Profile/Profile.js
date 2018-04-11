880bbec606a7dc3f111dc6bf24b03507a396c59c
Alex Beim <alexandergbeim@gmail.com>
2018-04-10 20:20:47 -0400

    added a few lines to Profile.js and css

--- client/src/pages/Profile/Profile.js
+++ client/src/pages/Profile/Profile.js
@@ -4,8 +4,23 @@ import Collapse from '../../components/collapse';
 
 class Profile extends Component {
   render() {
+    let name = "alex";
     return (
+      <div className = "container">
+        <div className = "name">
+          <h1> {name}'s Splash Page</h1>
+        </div>
 
+        <div className = "collapse">
+          <Collapse/>
+        </div>
+
+
+
+
+
+
+      </div>
     )
   }
 }
