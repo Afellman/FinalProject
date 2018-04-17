2acb955d10cdb8c7c725ae65da99a7682cd27d92
ALEXIS DELUCIA <alexisdelucia23@gmail.com>
2018-04-17 19:22:05 -0400

    took out nav bar animation/ still messing with nav bar colors

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -1,23 +1,49 @@
-import React from "react";
+import React, {Component} from "react";
 import "./Nav.css";
+import Logout from '../logout-btn';
+import { HamburgerVortex } from 'react-animated-burgers'
 
+class Nav extends Component {
+  constructor(props) {
+    super(props)
 
-const Nav = () =>
-  <nav className="navbar navbar-inverse navbar-top">
-    <div className="container-fluid">
-      <div className="navbar-header">
-    
-        <a href="/" className="navbar-brand">
-          Final Project
-        </a>
-      </div>
-      <ul className="nav navbar-nav navbar-right">
-        <li><a href="/">Login</a></li>
-        <li><a href="/saved">Sign-up</a></li>
-      </ul>
-    </div>
-  </nav>
+    this.state = {
+      isActive: false
+    }
 
+    this.toggleButton = this
+      .toggleButton
+      .bind(this)
+  }
 
+  toggleButton() {
+    this.props.showProfile()
+    this.setState({
+      isActive: !this.state.isActive
+    })
+  }
+  render() {
 
+    return (
+      <nav className="navbar navbar-inverse navbar-top">
+        <div className="container-fluid">
+          <div className="navbar-header">
+            <a href="/" className="navbar-brand">
+             Splash
+            </a>
+          </div>
+          <ul className="nav navbar-nav navbar-right">
+            <Logout/>
+            <HamburgerVortex
+              
+              isActive={this.state.isActive}
+              toggleButton={this.toggleButton}
+              buttonColor="rgb(223, 83, 157)"
+              barColor="white"/>
+          </ul>
+        </div>
+      </nav>
+    )
+  }
+}
 export default Nav;
